c40cbe539861097a447507b08d766e82c72a1a8f
Nicolas Rodrigue <nicolas.rodrigue.3@ens.etsmtl.ca>
2026-02-15 16:04:36 -0500

    wip

---
+++ PFE_mapper_node.py
@@ -0,0 +1,60 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import PointCloud2
+from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, LivelinessPolicy, HistoryPolicy
+
+class Mapper_Node(Node):
+
+    def __init__(self):
+        super().__init__('qvio_node')
+        qos_profile = QoSProfile(
+        reliability=ReliabilityPolicy.BEST_EFFORT,
+        durability=DurabilityPolicy.VOLATILE,
+        liveliness=LivelinessPolicy.AUTOMATIC,
+        depth=5,
+        history=HistoryPolicy.KEEP_LAST
+                                                                                    )
+        self.subscription = self.create_subscription(
+            PointCloud2,
+            '/mapper',
+            self.qvio_callback,
+            qos_profile=qos_profile)
+
+        self.get_logger().info('qvio node started')
+
+    def qvio_callback(self, msg : PointCloud2):
+        self.get_logger().info("callback received")
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    mapper_node = Mapper_Node()
+
+    rclpy.spin(mapper_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    mapper_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
